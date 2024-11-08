69b0b69f73fe024690c18ad9337c73dcbff89e57
Francesco Velluto <vellfr00@gmail.com>
2024-11-08 14:41:20 +0100

    Add cli support and provide host and port to web server from cli

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,56 @@
 mod entities;
 mod web_server;
+mod cli_client;
+
+use clap::{Parser, ValueEnum};
+use regex::Regex;
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+enum RunType {
+    /// Run the server
+    Server,
+    /// Run the client
+    Client
+}
+const DEFAULT_HOST: &str = "127.0.0.1";
+const DEFAULT_PORT: u16 = 3030;
+
+#[derive(Parser)]
+struct Cli {
+    /// Whether to run the server or the client
+    #[arg(long)]
+    run: RunType,
+    /// The host where server will run (if server) or the host to connect to (if client)
+    #[arg(long, value_parser = host_is_valid, default_value_t = DEFAULT_HOST.to_string())]
+    host: String,
+    /// The port where server will run (if server) or the port to connect to (if client)
+    #[arg(long, default_value_t = DEFAULT_PORT)]
+    port: u16,
+}
+
+const HOST_REGEX: &str = r"^(\d{1,3}\.){3}\d{1,3}$";
+fn host_is_valid(s: &str) -> Result<String, String> {
+    if Regex::new(HOST_REGEX).unwrap().is_match(s) {
+        Ok(s.to_string())
+    } else {
+        Err("Invalid host".to_string())
+    }
+}
 
 #[tokio::main]
 async fn main() {
-    let web_server = web_server::WebServer::new();
-    web_server.run().await;
+    let cli = Cli::parse();
+
+    let host = cli.host;
+    let port = cli.port;
+
+    match cli.run {
+        RunType::Server => {
+            let web_server = web_server::WebServer::new(&host, port);
+            web_server.run().await;
+        },
+        RunType::Client => {
+            println!("Client not implemented yet");
+        }
+    }
 }
--- src/web_server/mod.rs
+++ src/web_server/mod.rs
@@ -5,24 +5,28 @@ use crate::entities::server::Server;
 use std::sync::{Arc, Mutex};
 
 pub struct WebServer {
+    host: [u8; 4],
+    port: u16,
     pub server: Arc<Mutex<Server>>
 }
 
 impl WebServer {
-    pub fn new() -> WebServer {
+    pub fn new(host: &str, port: u16) -> WebServer {
+        let host_vec = host.split('.').map(|x| x.parse().unwrap()).collect::<Vec<u8>>();
+        let host_array: [u8; 4] = host_vec.try_into().expect("Host should have 4 octets");
+
         WebServer {
+            host: host_array,
+            port,
             server: Arc::new(Mutex::new(Server::new()))
         }
     }
 
     pub async fn run(&self) {
-        let address = [127, 0, 0, 1];
-        let port = 3030;
-
-        println!("{}", format!("Starting server at http://{}.{}.{}.{}:{}/", address[0], address[1], address[2], address[3], port));
+        println!("{}", format!("Starting server at http://{}.{}.{}.{}:{}/", self.host[0], self.host[1], self.host[2], self.host[3], self.port));
 
         warp::serve(routes::routes(self.server.clone()))
-            .run((address, port))
+            .run((self.host, self.port))
             .await;
     }
 }
