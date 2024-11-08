2a4e0bb5eb345d4727e48bcd6973644675d4b072
Francesco Velluto <vellfr00@gmail.com>
2024-11-08 13:47:38 +0100

    Removed remove user from room function

--- src/entities/room.rs
+++ src/entities/room.rs
@@ -34,15 +34,6 @@ impl Room {
         Ok(())
     }
 
-    pub fn remove_user_from_room(&mut self, user: Arc<User>) -> Result<(), &'static str> {
-        if !self.is_user_in_room(user.clone()) {
-            return Err("User is not in the room");
-        }
-
-        self.users.retain(|u| u.id != user.id);
-        Ok(())
-    }
-
     pub fn post_new_message(&mut self, message: Arc<Message>) -> Result<(), &'static str> {
         let author_user = message.author.clone();
         if !self.is_user_in_room(author_user) {
@@ -88,23 +79,6 @@ mod tests {
         room.add_user_to_room(user.clone()).unwrap();
         let result = room.add_user_to_room(user.clone());
         assert_eq!(result, Err("User is already in the room"));
-    }
-
-    #[test]
-    fn test_room_remove_user_from_room() {
-        let user = Arc::new(User::new("test".to_string()));
-        let mut room = Room::new("test".to_string());
-        room.add_user_to_room(user.clone()).unwrap();
-        room.remove_user_from_room(user.clone()).unwrap();
-        assert_eq!(room.users.len(), 0);
-    }
-
-    #[test]
-    fn test_room_remove_user_from_room_error() {
-        let user = Arc::new(User::new("test".to_string()));
-        let mut room = Room::new("test".to_string());
-        let result = room.remove_user_from_room(user.clone());
-        assert_eq!(result, Err("User is not in the room"));
     }
 
     #[test]
