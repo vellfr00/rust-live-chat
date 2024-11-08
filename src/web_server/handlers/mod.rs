8eb1f1828f6e784d17fbc6d20aa7e130ed4da3b3
Francesco Velluto <vellfr00@gmail.com>
2024-11-08 15:17:37 +0100

    Implement status check route, handler and test for web-server

--- src/web_server/handlers/mod.rs
+++ src/web_server/handlers/mod.rs
@@ -2,9 +2,26 @@ pub mod users;
 pub mod rooms;
 
 use serde::{Serialize, Deserialize};
+use warp::http::StatusCode;
+use std::convert::Infallible;
+use std::sync::{Arc, Mutex};
+use crate::entities::server::Server;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct ErrorDetailsResponse {
     pub error_id: String,
     pub error_message: String
+}
+
+pub async fn is_server_reachable(server: Arc<Mutex<Server>>) -> Result<impl warp::Reply, Infallible> {
+    let server = server.lock();
+    if server.is_err() {
+        let error_object = &ErrorDetailsResponse {
+            error_id: "SERVER_UNREACHABLE".to_string(),
+            error_message: "The server is unreachable".to_string()
+        };
+        return Ok(warp::reply::with_status(warp::reply::json(&serde_json::json!(error_object)), StatusCode::INTERNAL_SERVER_ERROR));
+    }
+
+    Ok(warp::reply::with_status(warp::reply::json(&serde_json::json!({"status": "OK__SERVER_REACHABLE"})), StatusCode::OK))
 }
--- src/web_server/routes/mod.rs
+++ src/web_server/routes/mod.rs
@@ -5,11 +5,46 @@ use crate::entities::server::Server;
 use std::sync::{Arc, Mutex};
 use warp::Filter;
 
+use super::handlers;
+
 pub fn routes(server: Arc<Mutex<Server>>) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
-    users::users_routes(server.clone())
+    is_server_reachable_route(server.clone())
+        .or(users::users_routes(server.clone()))
         .or(rooms::rooms_routes(server.clone()))
+}
+
+/**
+ * GET /status
+ * Returns 200 OK if the server is reachable, with a JSON response body
+ * If there is a problem with unwrapping server reference, returns 500 Internal Server Error
+ */
+fn is_server_reachable_route(server: Arc<Mutex<Server>>) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    warp::path("status")
+        .and(warp::get())
+        .and(with_server(server.clone()))
+        .and_then(handlers::is_server_reachable)
 }
 
 fn with_server(server: Arc<Mutex<Server>>) -> impl Filter<Extract = (Arc<Mutex<Server>>,), Error = std::convert::Infallible> + Clone {
     warp::any().map(move || server.clone())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use warp::http::StatusCode;
+
+    #[tokio::test]
+    async fn test_is_server_reachable_route() {
+        let server = Arc::new(Mutex::new(Server::new()));
+        let route = is_server_reachable_route(server.clone());
+
+        let response = warp::test::request()
+            .method("GET")
+            .path("/status")
+            .reply(&route)
+            .await;
+
+        assert_eq!(response.status(), StatusCode::OK);
+    }
 }
